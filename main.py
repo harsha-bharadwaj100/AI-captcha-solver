11c3adcec98067ca94f89ecb428786d51c588fe8
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-09-03 14:41:28 +0530

    Audio File Update - A Breakthorough!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import whisper
 
 model = whisper.load_model('base')
-result = model.transcribe('audio (2).mp3')
+result = model.transcribe('https://www.google.com/recaptcha/api2/payload/audio.mp3?p=06ADUVZwAAMwAY01n7xoHdgYIlmMlSqD5r0GuqfiO-0saMOYRFnVSNvZH3ZxayzvOxwcr-LeILxp9p1MQkNTdV6WMzOPlgtHHlTeNNDWD95TzClBHwQ5ZlZI4rV4AxiJsn6bo3nRmtyT0KeM1L_P8WdQsauE47waEj6Fsq2ZNQ1VkNIqAD9-5fBKbAkME5xnoAzuoMzeX0Vmr0&k=6LflndkbAAAAAMtC8hy8LXjRop4gJqLVz0S1OyJQ')
 
 with open('transcription.txt', 'w') as f:
     f.write(result['text'])
